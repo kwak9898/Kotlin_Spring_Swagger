efaf0300ffe35dcd9c46658f700a72d78589f20e
kwaktaemin <kwak9898@github.com>
2022-06-02 18:21:06 +0900

    수정

--- src/main/kotlin/com/example/kotlin_spring_swagger/KotlinSpringSwaggerApplication.kt
+++ src/main/kotlin/com/example/kotlin_spring_swagger/KotlinSpringSwaggerApplication.kt
@@ -1,11 +1,11 @@
 package com.example.kotlin_spring_swagger
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinSpringSwaggerApplication
 
 fun main(args: Array<String>) {
-	SpringApplication.run(KotlinSpringSwaggerApplication::class.java, *args)
+	runApplication<KotlinSpringSwaggerApplication>(*args)
 }
