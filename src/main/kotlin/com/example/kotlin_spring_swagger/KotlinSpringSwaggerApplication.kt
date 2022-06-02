3cc5e58f60eacc3a7d016a8b69ecb9d24412fdb1
kwaktaemin <kwak9898@github.com>
2022-06-02 15:32:10 +0900

    Swagger Test

---
+++ src/main/kotlin/com/example/kotlin_spring_swagger/KotlinSpringSwaggerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_swagger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSwaggerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringSwaggerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_spring_swagger/KotlinSpringSwaggerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_spring_swagger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSwaggerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
