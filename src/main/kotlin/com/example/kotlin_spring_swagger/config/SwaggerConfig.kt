dfd895e62dde3f03c59a748435c3a72cc6456f9b
kwaktaemin <kwak9898@github.com>
2022-06-02 17:03:33 +0900

    SwaggerConfig

--- src/main/kotlin/com/example/kotlin_spring_swagger/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/kotlin_spring_swagger/config/SwaggerConfig.kt
@@ -1,3 +1,38 @@
-package com.example.kotlin_spring_swagger.controller
+package com.example.kotlin_spring_swagger.config
 
-import com.example.kotlin_spring_swagger.bean.Greeting
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class ApplicationConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(getApiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.hendisantika.swagger.kotlin.controller"))
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    private fun getApiInfo(): ApiInfo {
+        val contact = Contact("example", "http://example.wordpress.com", "kwaktae1@gmail.com")
+        return ApiInfoBuilder()
+            .title("Example Api Title")
+            .description("Example Api Definition")
+            .version("1.0.0")
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+            .contact(contact)
+            .build()
+    }
+}
