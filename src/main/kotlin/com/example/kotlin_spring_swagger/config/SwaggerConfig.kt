6992d957daf32b3440e45ab714695814fa7c111b
kwaktaemin <kwak9898@github.com>
2022-06-02 15:51:19 +0900

    application.properties Add

---
+++ src/main/kotlin/com/example/kotlin_spring_swagger/config/SwaggerConfig.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin_spring_swagger.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
